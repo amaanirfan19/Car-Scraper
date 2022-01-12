8278d7ac11561ac265925b62858c6517adcda527
Amaan <amaanirfan19@gmail.com>
2022-01-11 19:21:12 -0500

    changed carScraper.ipynb

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import streamlit as st
 import pandas as pd
 import numpy as np
 from model import generate_model
--- model.py
+++ model.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
 from scraper import Scraper
 
 def generate_model(car_type = "sedan", num_pages = -1):
